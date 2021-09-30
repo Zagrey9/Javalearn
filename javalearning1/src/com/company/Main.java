502897ddecf30077838f422575b92f0224bee455
Zagrey9 <91551846+Zagrey9@users.noreply.github.com>
2021-09-30 06:02:50 +0400

    learning1
    learning1

--- javalearning1/src/com/company/Main.java
+++ javalearning1/src/com/company/Main.java
@@ -5,13 +5,13 @@ public class Main {
 
     public static boolean isSquare(int x) {
         int n = (int) Math.sqrt(x);
-        int r = Math.round(n) * Math.round(n);
+        int r = n * n;
         return r == x;
 
     }
 
     public static void main(String[] args) {
-        int x = 16;
+        int x = 17;
         if (isSquare(x)) System.out.println("Полный квардрат");
 
         else System.out.println("Неполный квардрат");
